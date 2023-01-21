1958828b8b581097017624970b274add72c64f2e
Syncxv <aruldevarackal1@gmail.com>
2023-01-22 02:59:34 +1100

    ok that was stupid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
 	};
 
 	async function onClick() {
-		reset();
+		// reset();
 		console.log('-----------------------------');
 		const target = instance.grid.flat().find((s) => s.isTarget)!;
 		const start = instance.grid.flat().find((s) => s.isStart)!;
